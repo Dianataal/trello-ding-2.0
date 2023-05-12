8a3608968816b69833a311c7d1832d4fa2f06fa3
Diana Taal <taaldiana@gmail.com>
2023-05-12 13:14:30 +0300

    some stuff works better

--- ding.js
+++ ding.js
@@ -8,39 +8,54 @@ const keyword = config.keyword;
 // see peaks checkima iga mõne aja tagant aga lisab selle siis, kui asi töötab
 // function checkChanges() {
 
-  fetch(`https://api.trello.com/1/boards/${board}/actions?key=${key}&token=${token}&filter=updateCard,updateList`)
-    .then(response => response.json())
-  // keyword list data only filter
-    .then(data => {
-      const filteredData = data.filter(action => {
+fetch(`https://api.trello.com/1/boards/${board}/actions?key=${key}&token=${token}`)
+  .then(response => response.json())
+  .then(data => {
+    const filteredData = data.filter(action => {
+      const listName = action.data.list ? action.data.list.name : '';
+      const cardName = action.data.card ? action.data.card.name : '';
+      return listName.includes(keyword) || cardName.includes(keyword);
+    });
+
+    filteredData.forEach(action => {
+      const listName = action.data.list ? action.data.list.name : '';
+      const cardName = action.data.card ? action.data.card.name : '';
+      console.log(`Card '${cardName}' was added to list '${listName}'.`);
+    });
+
+    data.forEach(action => {
+      const cardName = action.data.card ? action.data.card.name : '';
+      const prevList = action.data.listBefore ? action.data.listBefore.name : '';
+      const newList = action.data.listAfter ? action.data.listAfter.name : '';
+      if (action.type === 'createCard') {
+        console.log(`${cardName} was added to the list ${newList}.`);
+        // moving cards works!!!
+      } else if (action.type === 'updateCard' && newList.includes(keyword)) {
+        console.log(`${cardName} was moved from list ${prevList} to ${newList}.`);
+      } else if (action.type === 'updateCard' && !(newList.includes(keyword))) {
+      // prevList isnt showing up tho so it needs to be changed?
+        console.log(`${cardName} was removed from list ${prevList}.`);
+        // 
+      } else if (action.type === 'updateCard' && typeof action.closed !== 'undefined') {
         const listName = action.data.list ? action.data.list.name : '';
-        const cardName = action.data.card ? action.data.card.name : '';
-        return listName.includes(keyword) || cardName;
+        if (action.closed === true) {
+          console.log(`${cardName} was archived from list ${listName}.`);
+        } else if (action.closed === false) {
+          console.log(`${cardName} was returned to list ${listName}.`);
+        }
+      }
+    });
+
+    cards.forEach((card) => {
+      card.actions.forEach((action) => {
+        const shortId = action.data.idShort;
+        if (action.type === "deleteCard") {
+          console.log(`A card with id ${shortId} was deleted from the board.`);
+        }
       });
-      filteredData.forEach(action => {
-        const listName = action.data.list ? action.data.list.name : '';
-        const cardName = action.data.card ? action.data.card.name : '';
-        console.log(`Card '${cardName}' was added to list '${listName}'.`);
-      });
-    })
-    .catch(error => console.error(error));
-
-// uus kaart tehti, liigutati listi non-keyword listist?
-
-
-// Kaart liigub listide vahel
-
-
-// Kaart liigub keyword listist non-keyword listi ehk eemaldatakse
-
-
-// Kaart arhiveeritakse
-
-
-// Kaart taastatakse arhiivist
-
-
-// Kaart kustutatakse täielikult
+    });
+  });
+  // catch error is missing here?
 
 // }
 // setInterval(checkChanges, 10000);
